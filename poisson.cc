4605ddeea0fe029214e8fe8e6027c42c46a90790
lenjavollmer <lenja.vollmer@gmx.de>
2020-05-16 14:22:37 +0000

    blatt3_aufgabe1

--- poisson.cc
+++ poisson.cc
@@ -10,11 +10,16 @@ double poisson(double mu, int k) {
 int main() {
     using namespace std;
 
+    vector<int> zaehler(11);
 
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler [n_i] += 1;
+    }
+    for(int j=0;j<11;++j) {
+        cout << zaehler [j] << endl;
     }
     fin.close();
 }
