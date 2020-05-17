8dd5e6e01d01a59b61c66eea0a415de1619c7a3f
lenjavollmer <lenja.vollmer@gmx.de>
2020-05-17 15:04:16 +0000

    blatt3_aufgabe1

--- poisson.cc
+++ poisson.cc
@@ -8,18 +8,27 @@ double poisson(double mu, int k) {
 }
 
 int main() {
+
     using namespace std;
+    ifstream fin("datensumme.txt"); //Einlesedatei festlegen
+    ofstream fout("hist.txt");  //Ausgabedatei festlegen
 
-    vector<int> zaehler(11);
+    vector<int> zaehler(11);  //Vektor mit 11 Einträgen definieren
+    int n_i;  //Variable definieren
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-        zaehler [n_i] += 1;
+    for(int i = 0 ; i < 234 ; ++i) {  //Vorschleife starten, die die nächsten Befehle 234 mal ausführt
+        fin >> n_i; //aus Einlesedatei einlesen und den aktuell eingelesenen Wert nach n_i speichern
+        zaehler [n_i] += 1; //Vektoreintrag an der Stelle des eingelesenen Eintrags um einen erhöhen
     }
-    for(int j=0;j<11;++j) {
-        cout << zaehler [j] << endl;
+
+    for(int j=0;j<11;++j) { //Vorschleife starten, die die nächsten Befehle 11 mal ausführt
+        cout << zaehler [j] << endl;  //Vektoreintrag an j'ter Stelle ausgeben
+        fout <<j; //j in Ausgabedatei schreiben
+        fout <<" "; //leere Spalte in Ausgabedatei schreiben
+        fout <<zaehler [j]<< endl;  //Vektoreintrag an j'ter Stelle in Ausgabedatei schreiben
     }
+
     fin.close();
+    fout.close();
+
 }
