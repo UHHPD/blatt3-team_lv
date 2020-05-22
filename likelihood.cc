2f24bfa325f1e6b95fac9f6820f7c4ede6f347fd
lenjavollmer <lenja.vollmer@gmx.de>
2020-05-22 15:30:06 +0000

    blatt3_aufgabe2

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+using namespace std;
 
 
-double poisson(double mu, int k) {
-    return 0;
-}
+double mu=3.11538;  //Mittelwert
 
-int main() {
-    using namespace std;
+double prob(vector<int> daten, double mu) { //Funktion außerhalb des Hauptprogramms starten
+
+    double L=1; //Variable definieren
+
+    double e_mu = exp(-mu); //Implementation der Exponentialfunktion
+
+    for(int k : daten) {  //Vorschleife starten, die die nächsten Befehle mit allen Einträgen aus dem Vektor daten je einmal ausführt
+
+        double mu_k = pow(mu, k); //Implementation für Potenz
+        double k_f = tgamma(k+1); //Implementation für Fakultät
+
+        L *=  mu_k * e_mu * (1/(k_f));  //Likelihood (Wahrscheinlichkeit, die Daten mit einem bestimmten mu zu erhalten)
+
+    } //Vorschleife beenden
+
+    return L; //Variable zurücksetzen
+
+} //Funktion außerhalb des Hauptprogramms beenden
 
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
-}
+
+int main() {  //Hauptprogramm starten
+
+    
+    ifstream fin("datensumme.txt"); //Einlesedatei festlegen
+
+    vector<int> daten(0); //Vektor mit 0 Einträgen defineiren
+    int n_i;  //Variable definieren
+
+    for(int i = 0 ; i < 234 ; ++i) {  //Vorschleife starten, die folgenden Befehle 234 mal ausführt
+
+        fin >> n_i; //aus Einlesedatei einlesen und den aktuell eingelesenen Wert nach n_i speichern
+        daten.push_back(n_i); //den aktuell in n_i gespeicherten Wert dem Vektor hinzufügen
+
+    } //vorschleife beenden
+
+    fin.close();  //Einlesedatei schließen
+
+    cout << prob(daten , mu) << endl; //Likelihood für mu im Terminal ausgeben
+    
+
+} //Hauptprogramm beenden
+
